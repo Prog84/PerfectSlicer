761ffaba183c94636932a6dfbe676a5891f534e1
Prog84 <bvv198424@rambler.ru>
2021-02-15 17:02:26 +1000

    Add sliicer and dotween

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using BzKovSoft.ObjectSlicer;
+using BzKovSoft.ObjectSlicerSamples;
+
+
+public class Slicer : MonoBehaviour
+{
+    [SerializeField] private GameObject _blade;
+    [SerializeField] private float _duration;
+    [SerializeField] private float _offsetY;
+    [SerializeField] private float _secondsBetweenSlice;
+
+    private float _elapsedTime = 0;
+
+    private BzKnife _knife;
+
+    private void Start()
+    {
+        _knife = _blade.GetComponentInChildren<BzKnife>();        
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (_elapsedTime >= _secondsBetweenSlice)
+            {
+                _elapsedTime = 0;
+
+                _knife.BeginNewSlice();
+
+                _blade.transform.DOMoveY(_blade.transform.position.y - _offsetY, _duration).SetLoops(2, LoopType.Yoyo);
+            }
+        }
+    }
+}
