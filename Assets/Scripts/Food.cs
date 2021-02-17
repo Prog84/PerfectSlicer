393f40a1608dd9a4ee7d5d9c077370d12e642f22
Prog84 <bvv198424@rambler.ru>
2021-02-17 10:48:25 +1000

    Add food script

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    [SerializeField] private int _score;
+    
+    private float _delayDestroy = 3f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Slicer slicer))
+        {
+            StartCoroutine(DestroyFood());
+        }
+    }
+
+    private IEnumerator DestroyFood()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_delayDestroy);
+            Debug.Log("Вы заработали: " + _score);
+            Destroy(gameObject);
+        }
+    }
+}
